3fcb3e0ebe789a375aac4fa7fa1aaa8e129d5cf5
avi tshuva <dharmax@gmail.com>
2013-03-11 21:30:04 +0200

    step 3

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 var app = angular.module('angularjs-starter', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.fontSize="2em";
   $scope.items = [ 
     { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
     { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
     { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
     { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
     ];
+  $scope.addEntry = function()   {
+    $scope.items.push( { name: $scope.name, gender: $scope.gender, occupation:$scope.occupation});
+  };
+  
 });
