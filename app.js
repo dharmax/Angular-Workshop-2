27ef6b82db1c4f545196ab3a928cb410a8842227
avi tshuva <dharmax@gmail.com>
2013-03-11 21:06:48 +0200

    step 2

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var app = angular.module('angularjs-starter', []);
 
 app.controller('MainCtrl', function($scope) {
-  $scope.name = 'World';
-  $scope.fontSize='2em';
+  $scope.fontSize="2em";
+  $scope.items = [ 
+    { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
+    { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
+    { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
+    { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
+    ];
 });
