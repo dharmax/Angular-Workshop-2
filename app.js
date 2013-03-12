5ab6ebb3e6cbed5495f8d645539c89ecbedebbe0
avi tshuva <dharmax@gmail.com>
2013-03-12 08:59:10 +0200

    baseline, step 5

---
+++ app.js
@@ -0,0 +1,29 @@
+var app = angular.module('angularjs-starter', []);
+
+app.factory( 'Db', function() {
+
+  var db = {};
+  var items = [ 
+    { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
+    { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
+    { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
+    { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
+    ];
+  db.getItems = function() { return items; };
+  db.add = function(item){ 
+    items.push(item);
+  };
+  
+  return db;
+});
+
+app.controller('MainCtrl', function($scope, Db) {
+  $scope.items = Db.getItems;
+  $scope.addEntry = function() {
+    Db.add( {name: $scope.name, gender:$scope.gender, occupation:$scope.occupation});
+  };
+  $scope.updateInfo = function(item) {
+    $scope.marked= item;
+  };
+  
+});
