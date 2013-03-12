e0d3275b2e407fef3c5ddb97f822df78e2f42c9b
avi tshuva <dharmax@gmail.com>
2013-03-12 09:02:11 +0200

    baseline, step 6

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
-var app = angular.module('angularjs-starter', []);
+angular.module('components', [])
+  .directive( 'user', function() {
+    return {
+      scope: {
+        user: "="
+      },
+      template:"<div> <img src='{{user.imageUrl}}'/>{{user.name}}</div>"
+    };
+  });
+
+var app = angular.module('angularjs-starter', ['components']);
 
 app.factory( 'Db', function() {
 
   var db = {};
   var items = [ 
-    { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
-    { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
-    { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
-    { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
+    { name: "Mr. Smith", gender: "male", occupation: "secret agent", imageUrl:"http://t3.gstatic.com/images?q=tbn:ANd9GcTFs9fRwPmz9ByS5aWk08IPJXATqKvR-J0dT1qFcD2HCefie6g6"},
+    { name: "Mrs. Smith", gender: "female", occupation: "secret agent", imageUrl:"http://t3.gstatic.com/images?q=tbn:ANd9GcTFs9fRwPmz9ByS5aWk08IPJXATqKvR-J0dT1qFcD2HCefie6g6"},
+    { name: "Harry Seldon", gender: "male", occupation: "fortune teller", imageUrl:"http://t3.gstatic.com/images?q=tbn:ANd9GcTFs9fRwPmz9ByS5aWk08IPJXATqKvR-J0dT1qFcD2HCefie6g6"},
+    { name: "Barak Obamba", gender: "male", occupation: "trouble maker", imageUrl:"http://t3.gstatic.com/images?q=tbn:ANd9GcTFs9fRwPmz9ByS5aWk08IPJXATqKvR-J0dT1qFcD2HCefie6g6"}
     ];
   db.getItems = function() { return items; };
   db.add = function(item){ 
