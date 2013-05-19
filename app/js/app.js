84061763444bedd12a7532b4f9fa9647d07100af
dharmax <dharmax@gmail.com>
2013-05-19 15:39:17 +0300

    removed: invalid references to controllers

--- app/js/app.js
+++ app/js/app.js
@@ -4,7 +4,7 @@
 // Declare app level module which depends on filters, and services
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: MyCtrl1});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: MyCtrl2});
+    $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: null});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: null});
     $routeProvider.otherwise({redirectTo: '/main'});
   }]);
