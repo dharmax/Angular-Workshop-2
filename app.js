7ba3e5f3b56e8eacd54b904f47a34a49bd18bc7c
dharmax <dharmax@gmail.com>
2013-05-19 14:27:55 +0300

    removed: clear

--- app.js
+++ app.js
@@ -3,28 +3,42 @@ var app = angular.module('angularjs-starter', []);
 app.factory( 'Db', function() {
 
   var db = {};
-  var items = [ 
+  var localdb = localStorage.app_db;
+  var defaultItems = [ 
     { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
     { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
     { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
     { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
-    ];
-
+  ];
+    
+  var items = localdb ? JSON.parse(localdb) : null;
+  
+  items = items || defaultItems;
   db.getItems = function() { return items; };
 
   db.add = function(item){ 
     items.push(item);
+    updateLocalDB();
   };
   
+  function updateLocalDB() {
+    localStorage["app_db"] = JSON.stringify(items);
+  }
+  db.clear = function() {
+    items = [];
+    updateLocalDB();
+  };
   return db;
 });
 
 app.controller('MainCtrl', function($scope, Db) {
-  $scope.items = Db.getItems;
+  $scope.items = function() {
+    return Db.getItems();
+  };
   $scope.addEntry = function() {
     Db.add( {name: $scope.name, gender:$scope.gender, occupation:$scope.occupation});
   };
-  $scope.updateInfo = function(item) {
-    $scope.marked= item;
+  $scope.clear = function() {
+    Db.clear();
   };
 });
