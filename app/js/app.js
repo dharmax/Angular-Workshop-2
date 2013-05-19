48619b968f99cd2e92bdeb0b1331b9957b887fc9
dharmax <dharmax@gmail.com>
2013-05-19 16:22:46 +0300

    removed dummy controller

--- app/js/app.js
+++ app/js/app.js
@@ -5,6 +5,6 @@
 angular.module('myApp', ['myApp.filters', 'myApp.services', 'myApp.directives']).
   config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/main', {templateUrl: 'partials/main.html', controller: MyController});
-    $routeProvider.when('/about', {templateUrl: 'partials/about.html', controller: function(){}});
+    $routeProvider.when('/about', {templateUrl: 'partials/about.html', null});
     $routeProvider.otherwise({redirectTo: '/main'});
   }]);
