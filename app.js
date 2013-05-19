ebe5265082d1d7fe91d81fa32b34943ff3f7b315
dharmax <dharmax@gmail.com>
2013-05-19 13:27:52 +0300

    prettier

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ var app = angular.module('angularjs-starter', []);
 app.factory( 'Db', function() {
 
   var db = {};
+
   var items = [ 
     { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
     { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
     { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
     { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
     ];
+
   db.getItems = function() { return items; };
+
   db.add = function(item){ 
     items.push(item);
   };
@@ -18,7 +21,9 @@ app.factory( 'Db', function() {
 });
 
 app.controller('MainCtrl', function($scope, Db) {
+
   $scope.items = Db.getItems;
+  
   $scope.addEntry = function() {
     Db.add( {name: $scope.name, gender:$scope.gender, occupation:$scope.occupation});
   };
