21fa426b0eae8f16507cc6a5bb3d0b4da0cd4b80
dharmax <dharmax@gmail.com>
2013-07-24 13:29:42 +0300

    removed: redundant (and problematic) div in the youtube player

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 var app = angular.module('angularjs-starter', []);
 
-app.controller('MainCtrl', function($scope) {
-  $scope.items = [ 
-    { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
-    { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
-    { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
-    { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
+app.controller('MainCtrl', function ($scope) {
+    $scope.items = [
+        { name: "Mr. Smith", gender: "male", occupation: "secret agent"},
+        { name: "Mrs. Smith", gender: "female", occupation: "secret agent"},
+        { name: "Harry Seldon", gender: "male", occupation: "fortune teller"},
+        { name: "Barak Obamba", gender: "male", occupation: "trouble maker"}
     ];
-  $scope.addEntry = function()   {
-    $scope.items.push( { name: $scope.name, gender: $scope.gender, occupation:$scope.occupation});
-  };
-  
+    $scope.addEntry = function () {
+        $scope.items.push({ name: $scope.name, gender: $scope.gender, occupation: $scope.occupation});
+    };
+    $scope.isMale = function(item) {
+        return item.gender==="male";
+    }
+
 });
